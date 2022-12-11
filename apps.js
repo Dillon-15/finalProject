4166cab31e05284ed1d9d7e9d7d3f0f33249586e
dillon-15 <dillonbarnett821@gmail.com>
2022-12-10 22:14:38 -0600

    Commented

--- apps.js
+++ apps.js
@@ -1,3 +1,7 @@
+// Code By Dillon Barnett
+// Vue Movie Tickets / Final Project
+
+// vue component for the navbar
 Vue.component('navbar', {
     template: `
     <div class="container" style="margin-left:auto; margin-right:auto; max-width: 98%;">
@@ -17,18 +21,14 @@ Vue.component('navbar', {
         </header>
 
     </div>`,
-    name: "navbar",
-    data(){
-        return{
-            
-        } 
-    }
+    name: "navbar"
 })
 
+// vue component for the sidebar
 Vue.component('sidebar',{
     template: `
     <div>
-        <button class=" custom-postion button" styles="width:60px;" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">
+        <button class=" custom-postion button sticky-top" styles="width:60px;" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">
             <i class="bi bi-cart-fill"></i>
         </button> 
         <div class="offcanvas offcanvas-end  custom-width" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
@@ -40,8 +40,11 @@ Vue.component('sidebar',{
                 <hr>
             </div>
             <div class="offcanvas-body">
-                <div container>
-                    <table>
+                <div class="container">
+                    <div id="empty" class="center-text">
+                        <h2>Cart is Empty</h2>
+                    </div>
+                    <table id="table">
                         <tr>
                             <th>Movie</th>
                             <th>Adult Tickets</th>
@@ -58,11 +61,11 @@ Vue.component('sidebar',{
                             <td><button v-on:click="remove" class="button" styles="font-size:20px; width:100px">Remove</button></td>
                         </tr>
 
-                        <tr>
-                            <td></td>
-                            <td></td>
+                        <tr v-for="tot in total">
                             <th>Total:</th>
-                            <td></td>
+                            <td>{{tot.adult}}</td>
+                            <td>{{tot.child}}</td>
+                            <td>{{tot.total}}</td>
                         </tr>
                         </table> 
                 </div>
@@ -71,25 +74,20 @@ Vue.component('sidebar',{
     </div>
     `,
     name: "sidebar",
-    // props:["msg", "movienames", "adulttickets", "childtickets", "subtotal"],
-    props:["msg", "ticket"],
-    data(){
-        return{
-
-        }
-    },
+    // In props: msg displays "Your Cart", ticket makes a tr for every ticket that is added to the cart, total updates the last table row with the new prices
+    props:["msg", "ticket", "total"],
+    // In methods: remove gets the table row that the button was clicked on and then removes it
     methods:{
         remove(e){
             console.log(e.target.parentElement.parentElement);
+            app.deletefromCart(e.target.parentElement.parentElement.innerHTML)
             var trRemove = e.target.parentElement.parentElement;
-            trRemove.remove()
+            trRemove.remove();
         }
     }
-
-
-
 })
 
+// vue component for the moviecard
 Vue.component('moviecard',{
     template: `
     <div class="card" style="width: 18rem;">
@@ -108,18 +106,21 @@ Vue.component('moviecard',{
                     <label for="numTicketC">Children:</label>
                     <input type="number" value="0" min="0" name="numTicketC" class="custom-input" placeholder="0">
                 </div>
-                <a v-on:click="addToCart" class="btn btn-primary addtoCart button">Add to cart</a>
+                <a v-on:click="addToCart" class="btn btn-dark addtoCart">Add to cart</a>
             </form>
 
         </div>
     </div>`,
     name: "moviecard",
+    // In props: movies makes a new div for every movie in movies and updates the title, image, and overview
     props:['movies'],
+    // In data: it sets img to the starting link and then to the poster_path
     data(){
         return{
             img: "https://image.tmdb.org/t/p/w500/" + this.movies.poster_path 
         } 
     },
+    // In methods: addToCart sets the cart table to visible and gets the inputs from the form and then sends the info to the array then calls add that is in app instance
     methods:{
         addToCart(e){
 
@@ -130,47 +131,59 @@ Vue.component('moviecard',{
             var movieName = ticketVal[0].parentElement.parentElement.parentElement.previousElementSibling.previousElementSibling.previousElementSibling.innerHTML;
             console.log(movieName)
 
+            document.getElementById("empty").style.visibility = "hidden";
+            document.getElementById("empty").style.height = "0px";
+            document.getElementById("table").style.visibility = "visible";
 
+            if (ticketVal[0].valueAsNumber != 0){
             var Atic = ticketVal[0].valueAsNumber + " x $13.00";
-            var Ctic = ticketVal[1].valueAsNumber + " x $9.00";
+            } else {
+                var Atic = " ";
+            }
+            if (ticketVal[1].valueAsNumber != 0){
+                var Ctic = ticketVal[1].valueAsNumber + " x $9.00";
+                } else {
+                    var Ctic = " ";
+                }
+
             var subTo = ticketVal[0].valueAsNumber * 13 + ticketVal[1].valueAsNumber * 9;
 
-            if (ticketVal[0].valueAsNumber != 0 || ticketVal[1].valueAsNumber != 0){
-                app.ticketObj.push(
-                    {
-                        "name": movieName,
-                        "aTicket": Atic,
-                        "cTicket": Ctic,
-                        "sub": subTo
-                    }
-                )
-            }
-
-
-
-
-
-            console.log(app.ticketObj)
-
+            
+            app.ticketObj.push(
+                {
+                    "name": movieName,
+                    "aTicket": Atic,
+                    "cTicket": Ctic,
+                    "sub": subTo
+                }
+            )
+            
+            app.add(ticketVal[0].valueAsNumber, ticketVal[1].valueAsNumber);
             
         }
     }
 
     })
 
+// the apikey for axios call
 var apikey = "df78dbf203b1cd1f3a6d506adc95f2fd";
 
+// new vue instance of vue
 const app = new Vue({
     el:"#app",
+    // In data: msg is sent to sideBar, movies is sent to moviecard, ticketObj is sent to sidebar, totalnum is sent to sidebar, Atick and Ctick and totals is used to get totalnum
     data: {
         msg: "Your Cart",
         movies:[],
         ticketObj:[],
-        // addedA:[],
-        // addedC:[],
-        // subTotal:[],
-        // ticketName:[]
+        totalnum:[],
+        Atick:0,
+        Ctick:0,
+        totals:0
+
+
     },
+    // In mounted: it uses axios to get the api call
     mounted(){
         axios
             .get(`https://api.themoviedb.org/3/movie/upcoming?api_key=${apikey}&language=en-US&page=1`)
@@ -178,4 +191,95 @@ const app = new Vue({
                 this.movies = responseU.data.results;
             })
     },
+    // In Methods: 
+    //  - add(): in this method it updates the totalnum array by adding values
+    //  - deletefromCart(): in this method it updates the total array by subtracting values
+    methods:{
+        add(Atotal, Ctotal){
+            // to get the full total of all adult and child tickets
+            this.Atick =  this.Atick + Atotal;
+            this.Ctick = this.Ctick + Ctotal;
+            this.totals = this.Atick + this.Ctick
+
+            // Calculates the full price for the adult and child sub totals and then adds them together to get new total price
+            var Aprice = this.Atick * 13;
+            var Cprice = this.Ctick * 9;
+            var Tprice = Aprice + Cprice;
+
+            // removes the last item in the array (in this case it removes the only thing in the array)
+            this.totalnum.pop()
+            // then pushs the new object into the array
+            this.totalnum.push(
+                {
+                    "adult": this.Atick + " @ $" + Aprice,
+                    "child": this.Ctick + " @ $" + Cprice,
+                    "total": "$" + Tprice
+                }
+            )
+
+            console.log(this.Ctick);
+            console.log(this.Atick);
+            console.log(this.totals);
+
+            console.log(this.totalnum);
+
+
+        },
+        deletefromCart(movieText){
+            // starts by getting the text form the table row that was removed and spliting it by </td>
+            var banana = movieText.split("</td>");
+            console.log(banana)
+
+            // in this step we are setting removeA and removeC to just the numbers that we want
+            var removeA = banana[1].slice(5,100).split(" ");
+            console.log(removeA);
+            var removeC = banana[2].slice(5,100).split(" ");
+            console.log(removeC);
+
+            // if it is an empty string then removeA[0] or removeC[0] is set to zero
+            if(removeA[0] == ""){
+                removeA[0] = 0;
+            }
+            if(removeC[0] == ""){
+                removeC[0] = 0;
+                
+            }
+
+            // to get the full total of all adult and child tickets
+            this.Atick =  this.Atick - parseInt(removeA[0]);
+            this.Ctick = this.Ctick - parseInt(removeC[0]);
+            this.totals = this.Atick + this.Ctick
+
+            // Calculates the full price for the adult and child sub totals and then adding them together to get new total price
+            var Aprice = this.Atick * 13;
+            var Cprice = this.Ctick * 9;
+            var Tprice = Aprice + Cprice;
+
+            // if the total price equals 0 then it displays cart is empty again
+            if (Tprice == 0){
+                document.getElementById("empty").style.visibility = "visible";
+                document.getElementById("empty").style.height = "36.7833px";
+                document.getElementById("table").style.visibility = "hidden";
+            }
+
+            // removes the last item in the array (in this case it removes the only thing in the array)
+            this.totalnum.pop()
+            // then pushs the new object into the array
+            this.totalnum.push(
+                {
+                    "adult": this.Atick + " @ $" + Aprice,
+                    "child": this.Ctick + " @ $" + Cprice,
+                    "total": "$" + Tprice
+                }
+            )
+
+            console.log(this.Ctick);
+            console.log(this.Atick);
+            console.log(this.totals);
+
+            console.log(this.totalnum);
+
+        }
+        
+    }
 })
